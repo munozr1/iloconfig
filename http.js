277763ef543b426c23d406e6c5d38f52f9a88ed4
Rodrigo Munoz <65474436+munozr1@users.noreply.github.com>
2021-12-08 17:39:37 +0000

    using axios over ilorest

--- http.js
+++ http.js
@@ -1,10 +1,13 @@
 const rest = require("ilorest");
 const iLO = rest.redfishClient("");
+const axios = require('axios');
+
 
 let iLO_host = "";
 let login_username = "";
 let login_password = "";
 
-function login(login_username, login_password) {
-	iLO.login();
+async function login(url, username, password){
+	return await axios.get(url, {username: username, password: password})
+	.then(console.log('LOGGED IN'));
 }
